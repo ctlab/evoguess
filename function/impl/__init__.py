dfc7e318b4dc558744ab3109aa4ee2f8a419f25a
alpavlenko <art-pavlenko@mail.ru>
2021-05-30 15:20:45 +0300

    remove shared version of gad function

--- function/impl/__init__.py
+++ function/impl/__init__.py
@@ -1,12 +1,9 @@
 from .gad import *
-from .sgad import *
 
 functions = {
     GuessAndDetermine.slug: GuessAndDetermine,
-    SharedGuessAndDetermine.slug: SharedGuessAndDetermine,
 }
 
 __all__ = [
     'GuessAndDetermine',
-    'SharedGuessAndDetermine'
 ]
