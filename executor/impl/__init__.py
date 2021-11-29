535ac9e3e39987ec32d2a392d0c1e7f91477d599
alpavlenko <art-pavlenko@mail.ru>
2021-11-29 16:30:13 +0300

    add thread mpi executor

--- executor/impl/__init__.py
+++ executor/impl/__init__.py
@@ -1,9 +1,11 @@
 from .thread_executor import *
 from .process_executor import *
+from .thread_mpi_executor import *
 from .process_mpi_executor import *
 
 executors = {
     ThreadExecutor.slug: ThreadExecutor,
     ProcessExecutor.slug: ProcessExecutor,
+    ThreadMPIExecutor.slug: ThreadMPIExecutor,
     ProcessMPIExecutor.slug: ProcessMPIExecutor
 }
---
+++ executor/impl/thread_mpi_executor.py
@@ -0,0 +1,34 @@
+from .._abc.executor import *
+
+try:
+    from mpi4py import MPI
+    from mpi4py.futures import as_completed
+    from mpi4py.futures.pool import ThreadPoolExecutor
+except ModuleNotFoundError:
+    pass
+
+
+class ThreadMPIExecutor(Executor):
+    slug = 'executor:thread:mpi'
+    name = "Executor: Thread MPI"
+
+    awaiter_dict = {
+        'as_completed': as_completed,
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mpi_size = MPI.COMM_WORLD.Get_size()
+        self.workers = max(1, self.mpi_size - 1)
+        self.executor = ThreadPoolExecutor(max_workers=self.workers)
+
+    def submit(self, fn, *args, **kwargs):
+        return self.executor.submit(fn, *args, **kwargs)
+
+    def shutdown(self, wait=True):
+        self.executor.shutdown(wait)
+
+
+__all__ = [
+    'ThreadMPIExecutor'
+]
