3131708210a181f2e735c222444f5f969c53a000
alpavlenko <art-pavlenko@mail.ru>
2021-11-30 13:38:27 +0300

    add mpi executor to init

--- executor/impl/__init__.py
+++ executor/impl/__init__.py
@@ -1,9 +1,11 @@
+from .mpi_executor import *
 from .thread_executor import *
 from .process_executor import *
 from .thread_mpi_executor import *
 from .process_mpi_executor import *
 
 executors = {
+    MPIExecutor.slug: MPIExecutor,
     ThreadExecutor.slug: ThreadExecutor,
     ProcessExecutor.slug: ProcessExecutor,
     ThreadMPIExecutor.slug: ThreadMPIExecutor,
