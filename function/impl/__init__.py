b88742d36fb8f6089eb2f7c64d6a70d243ca4f7a
alpavlenko <art-pavlenko@mail.ru>
2022-03-09 15:22:34 +0300

    add rho function

--- function/impl/__init__.py
+++ function/impl/__init__.py
@@ -1,18 +1,21 @@
 from .gad import *
 from .ibs import *
 from .up_gad import *
+from .ibs_rho import *
 from .incr_gad import *
 
 functions = {
     GuessAndDetermine.slug: GuessAndDetermine,
     InverseBackdoorSets.slug: InverseBackdoorSets,
     UPGuessAndDetermine.slug: UPGuessAndDetermine,
-    IncrGuessAndDetermine.slug: IncrGuessAndDetermine
+    IncrGuessAndDetermine.slug: IncrGuessAndDetermine,
+    RhoInverseBackdoorSets.slug: RhoInverseBackdoorSets,
 }
 
 __all__ = [
     'GuessAndDetermine',
     'InverseBackdoorSets',
     'UPGuessAndDetermine',
-    'IncrGuessAndDetermine'
+    'IncrGuessAndDetermine',
+    'RhoInverseBackdoorSets'
 ]
