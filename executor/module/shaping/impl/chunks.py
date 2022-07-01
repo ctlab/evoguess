aa95ea63a6eebeab47e24857d2381a0ec1342136
alpavlenko <art-pavlenko@mail.ru>
2022-07-01 11:53:29 +0300

    update chunk shaping logic

--- executor/module/shaping/impl/chunks.py
+++ executor/module/shaping/impl/chunks.py
@@ -8,17 +8,15 @@ class Chunks(Shaping):
     slug = 'shaping:chunks'
     name = 'Shaping: Chunks'
 
-    def __init__(self, chunk_rate, *args, **kwargs):
-        self.chunk_rate = chunk_rate
-        super().__init__()
+    def __init__(self, chunk_size, *args, **kwargs):
+        self.chunk_size = chunk_size
+        super().__init__(*args, **kwargs)
 
     def get(self, size, tasks):
-        chunks_count = max(1, self.chunk_rate * size)
-        chunk_size = max(1, int(len(tasks) // chunks_count))
-        return slice_by_size(chunk_size, tasks)
+        return slice_by_size(self.chunk_size, tasks)
 
     def __info__(self):
         return {
             **super().__info__(),
-            'chunk_rate': self.chunk_rate
+            'chunk_size': self.chunk_size
         }
