71cdb0f4b097de8f33824989bebdd943e0446ccc
alpavlenko <art-pavlenko@mail.ru>
2022-08-08 14:14:35 +0300

    remove old imports

--- instance/typings/__init__.py
+++ instance/typings/__init__.py
@@ -1,15 +1,11 @@
 from . import var
 from .operator import *
-from .cnf import AIG, CNF, CNFPlus
 
 from .interval import *
 from .backdoor import *
 from .variables import *
 
 types = {
-    AIG.slug: AIG,
-    CNF.slug: CNF,
-    CNFPlus.slug: CNFPlus,
     Backdoor.slug: Backdoor,
     Interval.slug: Interval,
     Variables.slug: Variables
